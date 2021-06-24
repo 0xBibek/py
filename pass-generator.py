28c37a45a5852776565900f3fc8a7d903804b6cd
Bibek <iambbk@yandex.com>
2021-06-24 18:16:37 +0545

    pass Gen

---
+++ pass-generator.py
@@ -0,0 +1,24 @@
+import time
+import random
+import secrets
+import requests
+import json
+import string
+
+
+
+password_length = 10
+possible_characters = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%&*"
+
+
+
+
+def passGen():
+	random_character_list = [random.choice(possible_characters) for i in range(password_length)]
+	random_password = "".join(random_character_list)
+	ranPass = random_password
+	randomp = 'Random Password: ' + ranPass
+	return randomp
+
+
+print(passGen())
